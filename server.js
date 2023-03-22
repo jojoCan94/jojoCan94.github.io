9f691c3197650bd2118bfcf3e812cbbd1ce66b2d
Jonathan Cannizzaro <j.cannizzaro@nexum-ai.com>
2023-03-22 16:34:12 +0100

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public'))); // Sostituisci 'public' con la directory che contiene i tuoi file HTML, CSS e altre risorse statiche, se diversa
+app.use(express.static(path.join(__dirname, '/'))); // Sostituisci 'public' con la directory che contiene i tuoi file HTML, CSS e altre risorse statiche, se diversa
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html')); // Sostituisci 'public' e 'index.html' con i nomi appropriati per il tuo progetto, se diversi
